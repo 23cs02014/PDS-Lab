01418219c4350138bd09cc0bf4b5a52df87ac804
23cs02014 <157111420+23cs02014@users.noreply.github.com>
2024-01-19 15:24:39 +0530

    Add files via upload

---
+++ 23CS02014_2_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15;
+    printf("++a%d",++a);
+    printf("\na++%d",a++);
+    printf("\n--a%d",--a);
+    printf("\na--%d",a--);
+
+    return 0;
+}
+
---
+++ 23CS02014_2_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a , b , c,max;
+    printf("Input Numbers:");
+    scanf("%d%d%d",&a,&b,&c);
+    (a>b && a>c)?
+    max = a: (b>a && b>c)?
+                max = b: (c>a && c>b)?
+                            max = c: printf("");
+
+
+printf("Max:%d",max);
+
+    return 0;
+}
